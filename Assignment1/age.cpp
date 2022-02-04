50dd84868c3cf94ac6ae1ffab67101e5a0e85d53
stand4rd <thomas.noble.jordan@gmail.com>
2022-02-04 14:58:10 -0800

    Uploading Assignment 1

--- Assignment1/age.cpp
+++ Assignment1/age.cpp
@@ -1,5 +1,12 @@
+/*
+* age.cpp is a program that computes the larget and smallest
+* value in an array of size 3. It first prompts the user for
+* three values, stores those values in an array, then in a for
+* loop, iterate over the array and find the largest and smallest
+* value.
+*/
+
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -11,6 +18,20 @@ int main() {
   cin >> ages[1];
   cin >> ages[2];
 
+  // sort
+  int largest = ages[0];
+  int smallest = ages[0];
+
+  for (int i = 0; i < 3; i++) {
+    if (ages[i] > largest) {
+      largest = ages[i];
+    }
+    if (ages[i] < smallest) {
+      smallest = ages[i];
+    }
+  }
+  cout << "Youngest: " << smallest << endl;
+  cout << "Oldest: " << largest << endl;
 
   return 0;
 }
