af32167f9595319d554f0785072b2b12ed099fd9
stand4rd <thomas.noble.jordan@gmail.com>
2022-03-02 16:57:16 -0800

    Created placeholder files

---
+++ Assignment4/mainprogram.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void setup(char format) { // put this in seperate file
+  if (format == 'r') {
+    // fill in random values
+  }
+  if (format == 'f') {
+    // take in user values from file
+  }
+}
+
+int main() {
+  char format;
+  cout << "Type r for random configuration or f from flat file." << endl;
+  cin >> format;
+  setup(format); // TODO
+
+  char boundry;
+  cout << "What kind of boundry mode would you like to run in? Enter m for mirror, d for donut, or n for normal" << endl;
+  cin >> boundry;
+
+
+  return 0;
+}
